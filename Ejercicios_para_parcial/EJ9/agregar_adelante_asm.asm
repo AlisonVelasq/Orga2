84dff9bd958821882d3071f1f154a9e3f8aa5ea7
AlisonVelasq <alison.velasquez.2001@gmail.com>
2022-09-20 22:17:43 -0300

    ej 9 casi temrinado

--- Ejercicios_para_parcial/EJ9/agregar_adelante_asm.asm
+++ Ejercicios_para_parcial/EJ9/agregar_adelante_asm.asm
@@ -1,24 +1,35 @@
 
 ;node_t* agregarAdelante_asm(int32_t valor, node_t* siguiente);
-;en rdi esta el puntero y en rsi el entero
+;en edi esta el entero y en rsi el puntero
 extern malloc
 section .text
-    global agregar_adelante_asm
+    global agregarAdelante_asm
 
-    agregar_adelante_asm:
+    agregarAdelante_asm:
         push rbp 
         mov rbp, rsp
         push r12
         push rbx
 
+        xor rax, rax
+        xor r12, r12
+        xor rbx, rbx
+
         ;tengo que crear un nuevo nodo y agregarlo adelante
+
         mov r12, rdi
+        mov rdi, 16
+        ;rdi la cant d ebute qu equiero reservar
         call malloc
         mov rbx, rax 
 
-        mov [rbx], r12 ;puntero al siguiente nodo
-        add rbx, 8
-        mov [rbx], rsi
 
+        mov [rbx], rsi ;primero guardo el puntero al siguiente nodo qu tiene 8 bytes
+        mov dword [rbx + 8], r12d ; guardo el nuevo valor, que tiene 4 bytes
+        ;primero tengo que guardar el valor(4 bytes) y luego el padding (4 bytes)
+
+        ;no tengo que hacer el paddiing por que ya reserve 16 bytes
+        pop rbx
+        pop r12
         pop rbp 
         ret 
--- Ejercicios_para_parcial/EJ9/lista.c
+++ Ejercicios_para_parcial/EJ9/lista.c
@@ -1,7 +1,7 @@
 #include "lista.h"
 
 node_t* agregarAdelante(int32_t valor, node_t* siguiente);
-//node_t* agregarAdelante_asm(int32_t valor, node_t* siguiente);
+extern node_t* agregarAdelante_asm(int32_t valor, node_t* siguiente);
 //int32_t valorEn(uint32_t indice, node_t* cabeza);
 //void destruirLista(node_t* cabeza);
 
@@ -9,10 +9,10 @@ int main(int argc, char* argv){
     //importante reservar malloc, por que si no solo tengo un puntero, que apunta a basura
     node_t* newnodo = (node_t*)malloc(16);
     newnodo->siguiente = NULL;
-    newnodo = agregarAdelante(1, newnodo);
-    newnodo = agregarAdelante(2, newnodo);
-    newnodo = agregarAdelante(3, newnodo);
-    newnodo = agregarAdelante(4, newnodo);
+    newnodo = agregarAdelante_asm(1, newnodo);
+    newnodo = agregarAdelante_asm(2, newnodo);
+    newnodo = agregarAdelante_asm(3, newnodo);
+    newnodo = agregarAdelante_asm(4, newnodo);
 
     for (int i = 0; i < 4; i++)
     {
