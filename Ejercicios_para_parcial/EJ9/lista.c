604e9b0f0c9b0ca914fc93ba7f75430d197be6e6
AlisonVelasq <alison.velasquez.2001@gmail.com>
2022-09-20 21:46:14 -0300

    ej 9 copilo c

--- Ejercicios_para_parcial/EJ9/lista.c
+++ Ejercicios_para_parcial/EJ9/lista.c
@@ -6,22 +6,26 @@ node_t* agregarAdelante(int32_t valor, node_t* siguiente);
 //void destruirLista(node_t* cabeza);
 
 int main(int argc, char* argv){
-    node_t* newnodo;
+    //importante reservar malloc, por que si no solo tengo un puntero, que apunta a basura
+    node_t* newnodo = (node_t*)malloc(16);
     newnodo->siguiente = NULL;
-    newnodo = agregarAdelante(1, &newnodo);
-    newnodo = agregarAdelante(2, &newnodo);
-    newnodo = agregarAdelante(3, &newnodo);
-    newnodo = agregarAdelante(4, &newnodo);
+    newnodo = agregarAdelante(1, newnodo);
+    newnodo = agregarAdelante(2, newnodo);
+    newnodo = agregarAdelante(3, newnodo);
+    newnodo = agregarAdelante(4, newnodo);
 
     for (int i = 0; i < 4; i++)
     {
-        printf("nodo %d : %d", i, newnodo->valor);
+        printf("nodo %d : %d\n", i, newnodo->valor);
+        newnodo = newnodo->siguiente;
     }
     
 }
 
 node_t* agregarAdelante(int32_t valor, node_t* siguiente){
-    node_t* auxnodo;
+    node_t* auxnodo = (node_t*)malloc(16);
+
+    
     auxnodo->siguiente = siguiente;
     auxnodo->valor = valor;
     return auxnodo;
