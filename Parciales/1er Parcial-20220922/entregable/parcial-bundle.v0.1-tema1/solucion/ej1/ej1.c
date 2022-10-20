12629b7cf2dbd33dde313149f2b88c5c40710b3c
AlisonVelasq <alison.velasquez.2001@gmail.com>
2022-10-20 01:09:28 -0300

    parcial 1 resuelto en C

---
+++ Parciales/1er Parcial-20220922/entregable/parcial-bundle.v0.1-tema1/solucion/ej1/ej1.c
@@ -0,0 +1,94 @@
+#include "ej1.h"
+
+char** agrupar_c(msg_t* msgArr, size_t msgArr_len){
+
+    //tengo que volver un vector de string, donde en cada posicion estaran
+    //los string concatenados (los del tag 0, tag1, etc)
+    //el vector a los sumo tiene cant_tag elementos, que para este ejer es 4
+
+    char** vectConcatenados = malloc(MAX_TAGS*8);
+
+    //tengo un vector que tiene direcciones de memoria y esas direcciones apuntan a strings
+
+    //para guardar cada string en vectConcatenados nesesito llamar a malloc
+    //y reservar para cada string, pero estos tamaños pueden ser totalmente distintos
+    //ya que podria nesetar mucho espacio solo para el tag 0 y los demas vacios, por ejemplo
+
+    //me creo un vector aux, para guardar la cant de bytes que nesecito para cada string concatenado
+    //recuendo que a lo sumo tengo 4 strings concatenados
+
+    //int* vectCantXtag = malloc(MAX_TAGS*sizeof(int)); //si lo hago asi tengo que asignar a cada lugar un cero
+    int vectCantXtag[MAX_TAGS] = {0,0,0,0}; //seria mejor un for por si quiero cambiar el el MAX_TAGS
+
+    //recorro el vector msgArr para saber cuanto reservar
+    
+    for(int i = 0; i < msgArr_len; i++){
+        if(msgArr[i].tag == 0){
+            //csads char es un byte, entonces reservo, text_len ( cant de char de cada string)
+            vectCantXtag[0] += (int)msgArr[i].text_len;
+        }
+        else if(msgArr[i].tag == 1){
+            vectCantXtag[1] += (int)msgArr[i].text_len;;
+        }
+        else if(msgArr[i].tag == 2){
+            vectCantXtag[2] += (int)msgArr[i].text_len;;
+        }
+        else if(msgArr[i].tag == 3){
+            vectCantXtag[3] += (int)msgArr[i].text_len;;
+        }
+    }
+
+    //ya tengo la cantidad a reservar para cada string concatenado
+    //ahora el vect auxiliar y reservo los bytes de cada string concatenado
+
+    for(int i = 0; i < MAX_TAGS; i++){
+        vectConcatenados[i] = malloc(vectCantXtag[i]+1);
+        //sumo 1 por el 0(nulo) que tienen al final los string, tiene que estar si o si
+    }
+
+    //ahora si, concateno
+    int com0 = 0; //creo la variables com = 0
+    int* dcom0 = &com0; //ontemgo la direccion de esa variable
+    //para desreferenciar com uso : *com
+
+    //hago lo mismo para el resto de indices(depende de la cant de tags) pero resumido
+    int dcom1 = 0;
+    int dcom2 = 0;
+    int dcom3 = 0;
+
+    for(int i = 0; i < msgArr_len; i ++){
+        if(msgArr[i].tag == 0){
+            concatenar(vectConcatenados[0], msgArr[i].text, (int)(msgArr[i].text_len), dcom0);
+        }
+        else if(msgArr[i].tag == 1){
+            concatenar(vectConcatenados[1], msgArr[i].text, (int)(msgArr[i].text_len), &dcom1);
+        }
+        else if(msgArr[i].tag == 2){
+            concatenar(vectConcatenados[2], msgArr[i].text, (int)(msgArr[i].text_len), &dcom2);
+        }
+        else if(msgArr[i].tag == 3){
+            concatenar(vectConcatenados[3], msgArr[i].text, (int)(msgArr[i].text_len), &dcom3);
+        }
+    }
+
+    return vectConcatenados;
+
+}
+//paso por "referencia" el string y el indice ya que quiero que estos si se modifiquen
+//i.e le paso la direccion donde esta la variable y modifico desde ahi
+
+void concatenar(char* sConcatenado, char* text, int text_len, int* indice){
+    int c = 0;
+    int j = 0;
+    int i = *indice;
+    //me guardo el indice desde donde tengo que concatenar?
+    for(j = i; j < i + text_len ; j++){
+        sConcatenado[j] = text[c];
+        c++;
+    }
+    *indice = j; //desde j por que copiamos hasta uno anterior a j 
+    //( j toma rl valor donde ya no es valida la guarda e.i una mas desde mi ultimo char guardadp )
+    
+    
+    
+}
---
+++ Parciales/1er Parcial-20220922/entregable/parcial-bundle.v0.1-tema1/solucion/ej1/main.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <assert.h>
+
+#include "ej1.h"
+//#include "ej1.c"
+
+int main (void){
+	/* Acá pueden realizar sus propias pruebas */
+/*
+	//pruebo definiendo un solo elemto en la struct, para esto llamo a malloc
+	//creo un puntero donde reservare memoria, como quiero una struct msg
+	//entonces guardo en msg_t* los 16 bytes de lña struct
+		
+	msg_t* prueba= (msg_t*) malloc(16);
+
+	(*prueba).text = "holaa";
+	(*prueba).text_len = 5;
+	(*prueba).tag = 0;
+
+	printf("%s \n", (*prueba).text);
+		printf("%i \n",(*prueba).text_len);
+		printf("%i \n", (*prueba).tag);
+	
+	
+	
+	//creo un vector de la struct msg_t
+	//msg_t* vectMsg = (msg_t*)malloc(3 * sizeof(msg_t*));  //server lugar par aun vector de 3 posiciones
+	//PREGUNTAR   parece que no tengo que reservar lugar para la cant de elem que va a tener mi vector
+	
+	
+	msg_t** vectMsg ;
+	(vectMsg[0]) = malloc(16); //reservbo en el primer lugar de ese vector 16 bytes de la struct
+	(vectMsg[1]) = malloc(16);
+	(vectMsg[2]) = malloc(16);
+	
+	
+	//es exactamente lo mismo hacerlo con (*vectMsg) o vectMsg[0]
+	//lo importante era reservar el lugar
+*/
+	//-----------LO DE ARRIBA NO, use msg_t** NO ERA LA IDEA
+
+	//creo un vector de tipo msg_t (no un vector de punteros de struct, como habia hecho arriba)
+	//y miro al vector como una tira larga, donde cada 24 bytes, empieza la siguiente posicion del vector
+	//asi qeu reservo de una vez el lugar, que es 3*24, para 3 posiciones
+
+	msg_t* vectMsg = (msg_t*)malloc(6 * sizeof(msg_t));
+
+	(vectMsg[0]).text = "holaa";
+	(vectMsg[0]).text_len = 5;
+	(vectMsg[0]).tag = 0;
+
+	(vectMsg[1]).text = " mundoo";
+	(vectMsg[1]).text_len = 7;
+	(vectMsg[1]).tag = 0;
+
+	(vectMsg[2]).text = " soy alison";
+	(vectMsg[2]).text_len = 11;
+	(vectMsg[2]).tag = 0;
+
+	(vectMsg[3]).text = "costo un";
+	(vectMsg[3]).text_len = 8;
+	(vectMsg[3]).tag = 2;
+
+	(vectMsg[4]).text = "bye <3";
+	(vectMsg[4]).text_len = 6;
+	(vectMsg[4]).tag = 3;
+
+	(vectMsg[5]).text = " huevo :(";
+	(vectMsg[5]).text_len = 9;
+	(vectMsg[5]).tag = 2;
+
+	//imprimo
+
+	for(int i = 0; i < 6; i++){
+
+		printf("%s \n", vectMsg[i].text);
+		printf("%i \n",vectMsg[i].text_len);
+		printf("%i \n", vectMsg[i].tag); 
+	}
+	
+	char** tagConcatenados = agrupar_c(vectMsg, 6);
+
+	for(int i = 0; i < MAX_TAGS; i++){
+
+		printf("%s \n", tagConcatenados[i]);
+
+	}
+	return 0;    
+}
+
+
