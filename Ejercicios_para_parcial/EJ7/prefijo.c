bbf6dae765d1edc33ab0713f974f7ec865025b1f
AlisonVelasq <alison.velasquez.2001@gmail.com>
2022-11-05 00:08:06 -0300

    ej 7 de tp parcial completo

--- Ejercicios_para_parcial/EJ7/prefijo.c
+++ Ejercicios_para_parcial/EJ7/prefijo.c
@@ -3,15 +3,18 @@
 #include <stdint.h>		//contiene la definición de tipos enteros ligados a tamaños int8_t, int16_t, uint8_t,...
 
 int32_t prefijo(char* s1, char* s2);
+char* quitar_prefijo(char* s1, char* s2);
 
 int main(int argc, char const *argv[]){
     
-    int32_t len = prefijo("pinchado", "pincel");
+    int32_t len = prefijo("Pinchado", "Pincel");
+    char* res = quitar_prefijo("Pinchado", "Pincel");
     printf("la longitud del prefijo es %d\n", len);
+    printf("quitando prefijo es: %s \n", res);
 }
 
-/*
-int prefijo(char* s1, char* s2){
+
+int prefijo_c(char* s1, char* s2){
     int tam = 6;
     int lenPrefijo = 0;
     for(int i = 0; i < tam; i++){
@@ -23,4 +26,29 @@ int prefijo(char* s1, char* s2){
         }
     }
     return lenPrefijo;
-}*/
+}
+char* quitar_prefijo_c(char* s1, char* s2){
+    //tengo que reservar los bytes para el char* de salida
+    //para eso nesito saber la longitud del prefijo y hago: long de s2 - long del prefijo, eso es lo que tengo que reservar
+    int32_t len_pre = prefijo(s1,s2);
+
+    //recorro con el for hasta llegar al indice de s2 desde donde quiero empezar a copiar
+    char* temp = s2;
+    for(int i = 0; i < len_pre; i++){
+        temp++;
+    }
+    //desde aca puedo copiar
+    int tam = 0;
+    
+    while((*temp) != 0){
+        tam++;
+        temp++;
+    }
+
+    char* res = malloc(tam);
+
+    for(int i = 0; i < tam; i++){
+        res[i] = s2[len_pre + i];
+    }
+    return res;
+}
--- Ejercicios_para_parcial/EJ7/prefijo_a.asm
+++ Ejercicios_para_parcial/EJ7/prefijo_a.asm
@@ -1,7 +1,10 @@
 
 section .text
     global prefijo
-
+    global quitar_prefijo
+    extern malloc
+;int prefijo_c(char* s1, char* s2
+;rdi:s1, rsi:s2
     prefijo:
         push rbp 
         mov rbp, rsp
@@ -29,6 +32,68 @@ section .text
         .fin
             mov eax, r8d
             pop rbp 
-            
         ret
+
+;char* quitar_prefijo_c(char* s1, char* s2)
+;rd1:s1, rsi:s2
+    quitar_prefijo:
+        push rbp 
+        push rbx
+        push r12
+        push r13
+        push r14
+        push r15
+        sub rsp, 1
+
+        mov rbp, rsp
+
+        mov r12, rdi
+        mov r13, rsi 
+        
+        ;los registros ya estan en rdi y rsi asi que llamo directamente
+        call prefijo
+        mov r14, rax
+
+        ;primer for para reservar lugar con malloc
+        mov r15, r13 ;creo un temp
+        mov rcx, r14
+        .ciclo:
+            inc r15
+            loop .ciclo
+        xor rbx, rbx
+        mov bx, word 0 ;aca voy a reservar el tam a reservar
+        .contador:
+            inc bx
+            inc r15
+            cmp [r15], byte 0
+            jne .contador 
+
+        inc bx ; incrmento para el char nulo
+        xor rdi, rdi
+        mov di, bx
+        call malloc ;ya no me sirve r12
+        mov r12, rax ;guardo el puntero a la salida
+
+        sub rbx, 1 ;resto 1 por que le agregue antes 1 por el nulo 
+        mov r15, r12 ; por que voya inc r15 y no puedo perder la dire
+        mov r9, 0 ; contador iesimo
+        add r13, r14 ; le añado el offset del prefijo
+        .for: 
+            mov r8b, [r13 + r9]
+            mov [r15], r8b  
+            inc r9
+            inc r15
+            cmp r9, rbx
+            jne .for 
+
+        mov rax, r12
+        .fin:
+        add rsp, 1
+        pop r15
+        pop r14
+        pop r13
+        pop r12
+        pop rbx
+        pop rbp 
+        ret 
 
